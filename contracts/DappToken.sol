f988894660e5a17c836c0fe2fae21014119a62e6
StudentOfJS <rodlewis45@gmail.com>
2018-05-24 15:08:51 +0200

    add transfer function

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -7,11 +7,27 @@ contract DappToken {
     string public standard = "dApp Token v1.0.0";
     // symbol
     uint256 public totalSupply;
+
+    event Transfer(
+        address indexed _from,
+        address indexed _to,
+        uint256 _value
+    );
     
     mapping(address => uint256) public balanceOf;
 
     constructor(uint256 _initialSupply) public {
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+        require(balanceOf[msg.sender] >= _value && _value > 0);
+        require(balanceOf[_to] >= 0);
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        emit Transfer(msg.sender, _to, _value);
+        return true;
     }
 }
--- test/DappToken.test.js
+++ test/DappToken.test.js
@@ -21,4 +21,29 @@ contract("DappToken", accounts => {
     assert(totalSupply.toNumber() < 1000001 && totalSupply.toNumber() > 999999)
     assert.equal(adminBalance.toNumber(), 1000000, "it allocates an initial supply to the admin account")
   })
+
+  it("transfers token ownership", async () => {
+    const tokenInstance = await DappToken.deployed()
+
+    try {
+      const assertMessage = await tokenInstance.transfer.call(accounts[1], 9999999999)
+      assert(false, "error message must contain revert")
+    } catch (error) {
+      assert.equal(error.message, "VM Exception while processing transaction: revert", "error message must contain revert")
+    }
+
+    const txReceipt = await tokenInstance.transfer(accounts[1], 25000, { from: accounts[0] })
+    const transferCall = await tokenInstance.transfer.call(accounts[1], 25000, { from: accounts[0] })
+    const balanceOfRecievingAccount = await tokenInstance.balanceOf(accounts[1])
+    const balanceOfSendingAccount = await tokenInstance.balanceOf(accounts[0])
+
+    assert.equal(balanceOfRecievingAccount.toNumber(), 25000, "adds the amount to the recieving account")
+    assert.equal(balanceOfSendingAccount.toNumber(), 1000000 - 25000, "deducts the amount from sending account")
+    assert.equal(txReceipt.logs.length, 1, "triggers one event")
+    assert.equal(txReceipt.logs[0].event, "Transfer", `should be the "Transfer" event`)
+    assert.equal(txReceipt.logs[0].args._from, accounts[0], "logs account tokens transfered from")
+    assert.equal(txReceipt.logs[0].args._to, accounts[1], "logs account tokens transfered to")
+    assert.equal(txReceipt.logs[0].args._value, 25000, "logs the transfer amount")
+    assert(transferCall, "transfer returns true")
+  })
 })

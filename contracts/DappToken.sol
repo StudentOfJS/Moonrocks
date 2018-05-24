bfd5bf16756f090d737391508d1e4775b856e102
StudentOfJS <rodlewis45@gmail.com>
2018-05-24 11:06:14 +0200

    initalize contract with correct values

--- contracts/DappToken.sol
+++ contracts/DappToken.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.4.23;
 
 contract DappToken {
-    // Constructor
-    // Set the total number of tokens
-    // Read the total number of tokens
+    // name
+    string public name = "dApp Token";
+    string public symbol = "DTU";
+    string public standard = "dApp Token v1.0.0";
+    // symbol
     uint256 public totalSupply;
     
-    constructor() public {
-        totalSupply = 1000000;
+    mapping(address => uint256) public balanceOf;
+
+    constructor(uint256 _initialSupply) public {
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
     }
 }
--- test/DappToken.test.js
+++ test/DappToken.test.js
@@ -2,10 +2,23 @@ const DappToken = artifacts.require("./DappToken.sol");
 
 
 contract("DappToken", accounts => {
-  it("sets the total supply upon deployment", async () => {
+  it("initializes the contract with the correct values", async () => {
+    const tokenInstance = await DappToken.deployed()
+    const tokenName = await tokenInstance.name()
+    const tokenSymbol = await tokenInstance.symbol()
+    const tokenStandard = await tokenInstance.standard()
+    assert.equal(tokenName, "dApp Token", "has the correct name")
+    assert.equal(tokenSymbol, "DTU", "has the correct symbol")
+    assert.equal(tokenStandard, "dApp Token v1.0.0", "has the correct standard")
+  })
+
+  it("sets and allocates the initial supply upon deployment", async () => {
     const tokenInstance = await DappToken.deployed()
     const totalSupply = await tokenInstance.totalSupply()
+    const adminBalance = await tokenInstance.balanceOf(accounts[0])
+
     assert.equal(totalSupply.toNumber(), 1000000, "sets total supply to 1m")
     assert(totalSupply.toNumber() < 1000001 && totalSupply.toNumber() > 999999)
+    assert.equal(adminBalance.toNumber(), 1000000, "it allocates an initial supply to the admin account")
   })
 })

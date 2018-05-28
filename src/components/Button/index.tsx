50b5bbfcc7fa7a015749ac0a93f2951016e484f1
StudentOfJS <rodlewis45@gmail.com>
2018-05-28 12:43:19 +0200

    Button sizing changes

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,35 @@
+import * as React from "react"
+import styled, { IThemeInterface } from "../../theme"
+
+interface IButtonProps {
+  theme?: IThemeInterface;
+  primary?: boolean;
+  className?: string;
+}
+
+const Button: React.StatelessComponent<IButtonProps> = props => (
+  <button className={props.className}>
+    {props.children}
+  </button>
+);
+
+
+export default styled(Button) `
+  background: ${props => props.theme.transparent ? props.theme.secondaryColor : props.theme.primaryColor};
+  color: ${props => props.theme.transparent ? props.theme.primaryColor : props.theme.secondaryColor};
+  max-width: 160px;
+  font-size: 1.1em;
+  font-family: ${props => props.theme.fontFamily};
+  margin: 1em;
+  padding: 0.4em 1em;
+  border: 2px solid ${props => props.theme.transparent ? props.theme.primaryColor : props.theme.secondaryColor};
+  border-radius: 5px;
+  transition: transform 800ms ease-in-out;
+  &:hover {
+    transition: transform 800ms ease-in-out;
+    cursor: pointer;
+    background: ${props => props.theme.transparent ? props.theme.primaryColor : props.theme.secondaryColor};
+    color: ${props => props.theme.transparent ? props.theme.secondaryColor : props.theme.primaryColor};
+    border: 2px solid ${props => props.theme.transparent ? props.theme.secondaryColor : props.theme.primaryColor};
+  }
+`

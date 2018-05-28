229efb90353de2a2a4437b9449def67621bbd058
StudentOfJS <rodlewis45@gmail.com>
2018-05-28 12:43:46 +0200

    Text components added

---
+++ src/components/Text/index.tsx
@@ -0,0 +1,26 @@
+import styled from "../../theme"
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  font-weight: 800;
+`;
+const SubTitle = styled.h2`
+  font-size: 1.3em;
+  text-align: center;
+  font-weight: 700;
+  font-style: oblique;
+`;
+const MinorTitle = styled.h3`
+  font-size: 1.2em;
+  font-weight: 600;
+  text-align: center;
+`;
+const Paragraph = styled.p`
+  font-size: 1em;
+  text-align: center;
+`;
+
+export {
+  Title, SubTitle, MinorTitle, Paragraph
+}

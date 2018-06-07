a2d920d00501b655f423c065aff48577a0dae0d8
StudentOfJS <rodlewis45@gmail.com>
2018-06-07 08:51:32 +0200

    Team folder

---
+++ src/components/Team/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface ITeamProps {
+  team: object
+}
+
+const Team: React.SFC<ITeamProps> = (props) => {
+  return (
+    <React.Fragment>
+      Team
+      </React.Fragment>
+  );
+};
+
+export default Team;

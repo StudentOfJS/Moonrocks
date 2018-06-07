97b3e7dadac6de514a46e62f1e1cc6c168c4cdea
StudentOfJS <rodlewis45@gmail.com>
2018-06-07 08:55:37 +0200

    whitepaper folder

---
+++ src/components/Whitepaper/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface IWhitepaperProps {
+  Whitepaper: object
+}
+
+const Whitepaper: React.SFC<IWhitepaperProps> = (props) => {
+  return (
+    <React.Fragment>
+      Whitepaper
+      </React.Fragment>
+  );
+};
+
+export default Whitepaper;

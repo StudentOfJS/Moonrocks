c07f74cce63a2653aef6dccac73d7c59365720e8
StudentOfJS <rodlewis45@gmail.com>
2018-06-07 08:54:15 +0200

    TokenSale folder

---
+++ src/components/TokenSale/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface ITokenSaleProps {
+  TokenSale: object
+}
+
+const TokenSale: React.SFC<ITokenSaleProps> = (props) => {
+  return (
+    <React.Fragment>
+      TokenSale
+      </React.Fragment>
+  );
+};
+
+export default TokenSale;

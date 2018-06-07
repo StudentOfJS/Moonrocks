bab5fbc26d88a7d2508586e9ca1947ca09edad84
StudentOfJS <rodlewis45@gmail.com>
2018-06-07 08:57:04 +0200

    roadmap folder

---
+++ src/components/Roadmap/index.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+interface IRoadmapProps {
+  Roadmap: object
+}
+
+const Roadmap: React.SFC<IRoadmapProps> = (props) => {
+  return (
+    <React.Fragment>
+      Roadmap
+      </React.Fragment>
+  );
+};
+
+export default Roadmap;

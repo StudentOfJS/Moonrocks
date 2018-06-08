bec161fcce0be9930ca8e03a59305d3460047cc8
StudentOfJS <rodlewis45@gmail.com>
2018-06-08 07:17:19 +0200

    added routes for roadmap team whitepaper

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,18 @@ import * as React from 'react';
 import { Route, Switch } from 'react-router-dom'
 import About from '../components/About'
 import Hero from '../components/Hero'
+import Roadmap from '../components/Roadmap'
+import Team from '../components/Team'
+import Whitepaper from '../components/Whitepaper'
 
 const Routes = () => {
   return (
     <Switch>
       <Route exact={true} path="/" component={Hero} />
       <Route exact={true} path="/about" component={About} />
+      <Route exact={true} path="/roadmap" component={Roadmap} />
+      <Route exact={true} path="/team" component={Team} />
+      <Route exact={true} path="/whitepaper" component={Whitepaper} />
     </Switch>
   )
 }

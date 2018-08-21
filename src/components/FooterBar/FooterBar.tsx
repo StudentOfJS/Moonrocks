80e6e42a23c92b3cc0020a8a5d29d5ddadd01386
StudentOfJS <rodlewis45@gmail.com>
2018-08-21 09:48:58 +0200

    change layout

--- src/components/FooterBar/FooterBar.tsx
+++ src/components/FooterBar/FooterBar.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { animateScroll as scroll } from "react-scroll";
 import styled, { keyframes } from "../../theme";
 import Div from "../Div";
-import { RocketIcon } from "../icons";
+import { FatRocketIcon } from "../icons";
 
 const preLaunch = keyframes`
 {
@@ -52,116 +52,38 @@ const preLaunch = keyframes`
   }
 }
 `;
-const liftOff = keyframes`
-{
-  0% {
-    -webkit-transform: translate(0);
-            transform: translate(0);
-  }
-  5% {
-    -webkit-transform: translate(-2px, -2px);
-            transform: translate(-2px, -2px);
-  }
-  10% {
-    -webkit-transform: translate(2px, -2px);
-            transform: translate(2px, -2px);
-  }
-  15% {
-    -webkit-transform: translate(-2px, 2px);
-            transform: translate(-2px, 2px);
-  }
-  20% {
-    -webkit-transform: translate(2px, 2px);
-            transform: translate(2px, 2px);
-  }
-  25% {
-    -webkit-transform: translate(-2px, -2px);
-            transform: translate(-2px, -2px);
-  }
-  30% {
-    -webkit-transform: translate(2px, -2px);
-            transform: translate(2px, -2px);
-  }
-  35% {
-    -webkit-transform: translate(-2px, 2px);
-            transform: translate(-2px, 2px);
-  }
-  40% {
-    -webkit-transform: translate(-2px, -2px);
-            transform: translate(-2px, -2px);
-  }
-  45% {
-    -webkit-transform: translate(2px, -2px);
-            transform: translate(2px, -2px);
-  }
-  50% {
-    -webkit-transform: translateY(0) rotateX(0) scale(1);
-            transform: translateY(0) rotateX(0) scale(1);
-    -webkit-transform-origin: 50% 1400px;
-            transform-origin: 50% 1400px;
-    opacity: 1;
-  }
-  100% {
-    -webkit-transform: translateY(-600px) rotateX(-30deg) scale(0);
-            transform: translateY(-600px) rotateX(-30deg) scale(0);
-    -webkit-transform-origin: 50% 100%;
-            transform-origin: 50% 100%;
-    opacity: 1;
-  }
-}
-`;
 
 const RocketDiv = styled.div`
+  align-self: flex-end;
+  bottom: 1rem;
+  position: sticky;
+  padding-right: 1rem;
   &:hover {
     animation: ${preLaunch} 0.5s linear infinite both;
     cursor: pointer;
   }
 `;
 
-const RocketLaunchDiv = styled.div`
-  animation: ${liftOff} 0.7s ease-in both;
-`;
-
-const BarItem = styled.div`
-  align-self: flex-end;
-  bottom: 1rem;
-  padding-top: 100vh;
-  position: sticky;
-`;
-
 const Bar = styled(Div)`
   align-items: flex-start;
   background: transparent;
   display: flex;
-  height: 100%;
-  justify-content: space-around;
+  height: 450vh;
+  justify-content: flex-end;
   top: 0;
   width: 100%;
-  z-index: 200;
+  z-index: 400;
 `;
 
 export interface IRocketProps {
   launch: boolean;
 }
 
-const Rocket: React.SFC<IRocketProps> = ({ launch }) => {
-  if (launch) {
-    return (
-      <BarItem>
-        <RocketLaunchDiv>
-          <RocketIcon size={32} color="#0892D0" />
-        </RocketLaunchDiv>
-      </BarItem>
-    );
-  }
-  return (
-    <BarItem>
-    <RocketDiv>
-      <RocketIcon size={32} color="white" />
-    </RocketDiv>
-  </BarItem>
-  );
-};
+const Rocket: React.SFC<IRocketProps> = ({ launch }) => (
+  <RocketDiv>
+    <FatRocketIcon size={32} color={launch ? "tomato" : "white"} />
+  </RocketDiv>
+);
 
 export interface IState {
   launch: boolean;
@@ -173,12 +95,7 @@ export interface IScrollProps {
   smooth: boolean;
 }
 
-
-
-export default class FooterBar extends React.Component<
-  {},
-  IState
-> {
+export default class FooterBar extends React.Component<{}, IState> {
   public state = {
     launch: false
   };
@@ -186,9 +103,10 @@ export default class FooterBar extends React.Component<
     this.setState({ launch: true });
     scroll.scrollToTop({
       delay: 700,
-      duration: 1500,
+      duration: 1000,
       smooth: true
     });
+    setTimeout(() => this.setState({ launch: false }), 1700);
   };
   public render() {
     return (

c45157d7c0cb0b9dbd9d95ff64ce085b124fd9f7
StudentOfJS <rodlewis45@gmail.com>
2018-08-28 11:38:39 +0200

    remove submit prop from Signup component

--- src/components/About/index.tsx
+++ src/components/About/index.tsx
@@ -78,8 +78,6 @@ export default () => {
             <Signup
               placeholder="Enter your email..."
               type="email"
-              // tslint:disable-next-line:jsx-no-lambda
-              submit={v => null}
               btnText="Signup"
             />
           </TimeWrap>
